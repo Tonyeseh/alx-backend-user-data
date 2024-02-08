d990e72453fae78c7f8d8141275e316b787ea4a2
Tonyeseh <esehtony123@gmail.com>
2024-02-08 11:18:15 +0100

    fix lint errors

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -8,11 +8,17 @@ from typing import List
 
 PII_FIELDS = ('ssn', 'password', 'email', 'phone', 'name')
 
-def filter_datum(fields: [List[str]], redaction: str, message: str, separator: str) -> str:
+
+def filter_datum(
+        fields: [
+            List[str]],
+    redaction: str,
+    message: str,
+        separator: str) -> str:
     """returns the log message obfuscated"""
     for field in fields:
         pattern = r'({}=)([^{}]+)'.format(field, separator)
-        
+
         message = re.sub(pattern, r'\1{}'.format(redaction), message)
 
     return message
@@ -27,11 +33,17 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
+        """ Initialize ReactingFormatter"""
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        record.msg = filter_datum(self.fields, self.REDACTION, record.getMessage(), self.SEPARATOR)
+        """Formats the log record"""
+        record.msg = filter_datum(
+            self.fields,
+            self.REDACTION,
+            record.getMessage(),
+            self.SEPARATOR)
 
         return super().format(record)
 
@@ -57,9 +69,14 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
     host = os.getenv('PERSONAL_DATA_DB_HOST', 'localhost')
     db_name = os.getenv('PERSONAL_DATA_DB_NAME', '')
 
-    connection = mysql.connector.connect(user=user_name, password=password, host=host, database=db_name)
+    connection = mysql.connector.connect(
+        user=user_name,
+        password=password,
+        host=host,
+        database=db_name)
 
     return connection
+
 
 def main():
     """logs the information about user records in a table"""
@@ -72,9 +89,13 @@ def main():
         cursor.execute(query)
         rows = cursor.fetchall()
         for row in rows:
-            record = map(lambda x: '{}={}'.format(x[0], x[1]), zip(columns, row))
+            record = map(
+                lambda x: '{}={}'.format(
+                    x[0], x[1]), zip(
+                    columns, row))
             msg = '{};'.format('; '.join(list(record)))
-            log_record = logging.LogRecord('user_data', logging.INFO, None, None, msg, None, None)
+            log_record = logging.LogRecord(
+                'user_data', logging.INFO, None, None, msg, None, None)
             logger.handle(log_record)
 
 
