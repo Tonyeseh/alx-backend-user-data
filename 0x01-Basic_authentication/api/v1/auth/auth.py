d0ae2939cdc79b27820f22541bdaf5043111137a
Tonyeseh <esehtony123@gmail.com>
2024-02-14 13:13:04 +0100

    task 3-6 0x01'

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+"""auth module"""
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """A simple authentication class """
+
+    def require_auth(self, path: str, exclude_paths: List[str]) -> bool:
+        """returns True if the path is not in the list of strings exclude_paths"""
+        if not path or not exclude_paths: return True
+
+        if not path.endswith('/'):
+            path = path + '/'
+
+
+        if path in exclude_paths:
+            return False
+
+        return True
+    
+    def authorization_header(self, request=None) -> str:
+        """returns None for now"""
+        if not request: return None
+
+        auth_header = request.headers.get('Authorization')
+        print('auth_header', auth_header)
+
+        if auth_header is None:
+            return None
+        
+        return auth_header
+    
+    def current_user(self, request=None) -> TypeVar('User'):
+        """returns None for now"""
+        return None
---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+"""basic_auth module"""
+
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """Basic authentication class"""
+    
