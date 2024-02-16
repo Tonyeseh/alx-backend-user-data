aba08a72b534dc0ece682b3102bd39091c7b86dc
Tonyeseh <esehtony123@gmail.com>
2024-02-16 12:35:36 +0100

    fixed lint error in auth.py 0x02

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -47,7 +47,7 @@ class Auth:
         """returns a cookie value from a request"""
         if request is None:
             return None
-        
+
         session_name = os.getenv('SESSION_NAME', '_my_session_id')
 
         return request.cookies.get(session_name, None)
